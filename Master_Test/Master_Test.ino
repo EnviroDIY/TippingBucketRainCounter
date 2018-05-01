cf90f106199f74f82d29dc343927b2f3a7b38db0
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2018-05-01 17:37:35 -0500

    Bug fix in Master_Test example, to get byte order correct
    Just as you suspected @bschulz1701 ! Thanks.

--- Master_Test/Master_Test.ino
+++ Master_Test/Master_Test.ino
@@ -23,25 +23,25 @@ THIS CODE IS PROVIDED "AS IS" - NO WARRANTY IS GIVEN.
 
 #include <Wire.h>
 
-unsigned long UpdateRate = 2000; //Number of ms between serial prints, 2000 by default
+unsigned long UpdateRate = 4000; //Number of ms between serial prints, 4 seconds by default
 uint8_t ADR = 0x08; //Address of slave device, 0x08 by default
 
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
-  Serial.begin(9600);  // start serial for output
+  Serial.begin(57600);  // start serial for output
   Serial.print("Welcome to the Machine...\n\n"); //Genaric begin statment for monitor
 }
 
 void loop() {
   unsigned int Tips = 0; //Used to measure the number of tips
-  uint8_t ByteHigh = 0; //Bytes to read then concatonate
-  uint8_t ByteLow = 0; 
+  uint8_t Byte1 = 0; //Bytes to read then concatonate
+  uint8_t Byte2 = 0;
 
-  Wire.requestFrom(8, 2);    // request 2 bytes from slave device #8
-  ByteHigh = Wire.read();  //Read number of tips back
-  ByteLow = Wire.read();
+  Wire.requestFrom(ADR, 2);    // request 2 bytes from slave device #8
+  Byte1 = Wire.read();  //Read number of tips back
+  Byte2 = Wire.read();
 
-  Tips = ((ByteHigh << 8) | ByteLow); //Concatonate bytes
+  Tips = ((Byte2 << 8) | Byte1); //Concatenate bytes
 
   Serial.print("Number of Tips since last read = ");
   Serial.println(Tips);  //Prints out tips to monitor
